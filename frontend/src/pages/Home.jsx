131522a1291f6cf3794925eeaa65fe876b9f4111
salvadev-sketch <tuyisengeemmanuel526@gmail.com>
2026-07-24 18:12:53 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Hero from '../components/home/Hero.jsx';
+import FeaturedBanner from '../components/home/FeaturedBanner.jsx';
 import CategoryStrip from '../components/home/CategoryStrip.jsx';
 import FeaturedProducts from '../components/home/FeaturedProducts.jsx';
 import StatsBar from '../components/home/StatsBar.jsx';
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedBanner />
       <CategoryStrip />
       <FeaturedProducts />
       <StatsBar />
